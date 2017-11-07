da436feba4e258c22db62b5fd501f6708335dbed
L117student <L117student@L117-18-043663.bitnet.wctc.edu>
2017-11-06 21:40:33 -0600

    Added More Java Doc Comments Minor Method Tweeking

--- src/edu/wctc/advjava/bmd/datetime/DateUtilites8.java
+++ src/edu/wctc/advjava/bmd/datetime/DateUtilites8.java
@@ -5,13 +5,12 @@
  */
 package edu.wctc.advjava.bmd.datetime;
 
-import java.time.DateTimeException;
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
-import java.time.temporal.TemporalField;
 import java.time.temporal.TemporalUnit;
 import java.time.temporal.UnsupportedTemporalTypeException;
 
@@ -46,26 +45,19 @@ public class DateUtilites8 {
     public DateUtilites8(){
         defaultPattern = SHORT_DATE_TIME;
     }
-    
-    public final int getTemporalFieldBetween(LocalDateTime date1, LocalDateTime date2, TemporalField unit)
-                throws DateTimeException,UnsupportedTemporalTypeException,ArithmeticException,IllegalArgumentException {
-       if(date1 == null || date2 == null || unit == null) throw new IllegalArgumentException("Arguments May Not Be Null");
-       if(date1.isAfter(date2)){LocalDateTime swap = date1;date1 = date2;date2 = swap;}//Swap Dates
-       return date1.get(unit) - date2.get(unit); 
-    }
-    
+        
     /**
      * Takes in two LocalDateTime Objects and a TemporalUnit and calculates the value between them.
      * Method simply preforms fromDate.until(toDate,unit) while insuring fromDate.isAfter(toDate)
      * and swaps arguments if condition is false.
      * 
-     * @param fromDate - first Date Object
-     * @param toDate - second Date Object
-     * @param unit - The TemporalUnit to be calculated from date objects
-     * @return - a long value that is representative of the TemproalUnit between fromDate - toDate
-     * @throws UnsupportedTemporalTypeException - if the unit is not supported
-     * @throws ArithmeticException - if numeric overflow occurs
-     * @throws IllegalArgumentException - Thrown if any of the arguments are null
+     * @param fromDate first Date Object
+     * @param toDate second Date Object
+     * @param unit The TemporalUnit to be calculated from date objects
+     * @return a long value that is representative of the TemproalUnit between fromDate - toDate
+     * @throws UnsupportedTemporalTypeException if the unit is not supported
+     * @throws ArithmeticException if numeric overflow occurs
+     * @throws IllegalArgumentException Thrown if any of the arguments are null
      */
     public final long getTemporalUnitBetween(LocalDateTime fromDate, LocalDateTime toDate, TemporalUnit unit)
                 throws UnsupportedTemporalTypeException,ArithmeticException,IllegalArgumentException {
@@ -77,12 +69,11 @@ public class DateUtilites8 {
     /**
      * Method calls getTemporalUnitBetween(fromDate,toDate,unit) with ChrnoUnit.YEARS argument for unit
      * 
-     * @param date1 - first date Object
-     * @param date2 - second date Object
-     * @return - a long that represent the years between date1 and date2
-     * @throws ArithmeticException - if numeric overflow occurs
-     * @throws IllegalArgumentException - Thrown if any of the arguments are null
-     * @see getTemporalUnitBetween(fromDate,toDate,unit)
+     * @param date1 first date Object
+     * @param date2 second date Object
+     * @return a long that represent the years between date1 and date2
+     * @throws ArithmeticException if numeric overflow occurs
+     * @throws IllegalArgumentException Thrown if any of the arguments are null
      */
     public final long getYearsBetween(LocalDateTime date1, LocalDateTime date2)
                 throws ArithmeticException,IllegalArgumentException {
@@ -92,12 +83,11 @@ public class DateUtilites8 {
      /**
      * Method calls getTemporalUnitBetween(fromDate,toDate,unit) with ChrnoUnit.MONTHS argument for unit
      * 
-     * @param date1 - first date Object
-     * @param date2 - second date Object
-     * @return - a long that represent the months between date1 and date2
-     * @throws ArithmeticException - if numeric overflow occurs
-     * @throws IllegalArgumentException - Thrown if any of the arguments are null
-     * @see getTemporalUnitBetween(fromDate,toDate,unit)
+     * @param date1 first date Object
+     * @param date2 second date Object
+     * @return a long that represent the months between date1 and date2
+     * @throws ArithmeticException if numeric overflow occurs
+     * @throws IllegalArgumentException Thrown if any of the arguments are null
      */
     public final long getMonthsBetween(LocalDateTime date1, LocalDateTime date2)
                 throws ArithmeticException,IllegalArgumentException {
@@ -107,12 +97,11 @@ public class DateUtilites8 {
      /**
      * Method calls getTemporalUnitBetween(fromDate,toDate,unit) with ChrnoUnit.DAYS argument for unit
      * 
-     * @param date1 - first date Object
-     * @param date2 - second date Object
-     * @return - a long that represent the days between date1 and date2
-     * @throws ArithmeticException - if numeric overflow occurs
-     * @throws IllegalArgumentException - Thrown if any of the arguments are null
-     * @see getTemporalUnitBetween(fromDate,toDate,unit)
+     * @param date1 first date Object
+     * @param date2 second date Object
+     * @return a long that represent the days between date1 and date2
+     * @throws ArithmeticException if numeric overflow occurs
+     * @throws IllegalArgumentException Thrown if any of the arguments are null
      */
     public final long getDaysBetween(LocalDateTime date1, LocalDateTime date2)
                 throws ArithmeticException,IllegalArgumentException {
@@ -122,12 +111,11 @@ public class DateUtilites8 {
      /**
      * Method calls getTemporalUnitBetween(fromDate,toDate,unit) with ChrnoUnit.MINUTES argument for unit
      * 
-     * @param date1 - first date Object
-     * @param date2 - second date Object
-     * @return - a long that represent the minutes between date1 and date2
-     * @throws ArithmeticException - if numeric overflow occurs
-     * @throws IllegalArgumentException - Thrown if any of the arguments are null
-     * @see getTemporalUnitBetween(fromDate,toDate,unit)
+     * @param date1 first date Object
+     * @param date2 second date Object
+     * @return a long that represent the minutes between date1 and date2
+     * @throws ArithmeticException if numeric overflow occurs
+     * @throws IllegalArgumentException Thrown if any of the arguments are null
      */
     public final long getMinutesBetween(LocalDateTime date1, LocalDateTime date2)
                 throws ArithmeticException,IllegalArgumentException {
@@ -137,12 +125,11 @@ public class DateUtilites8 {
     /**
      * Method calls getTemporalUnitBetween(fromDate,toDate,unit) with ChrnoUnit.HOURS argument for unit
      * 
-     * @param date1 - first date Object
-     * @param date2 - second date Object
-     * @return - a long that represent the hours between date1 and date2
-     * @throws ArithmeticException - if numeric overflow occurs
-     * @throws IllegalArgumentException - Thrown if any of the arguments are null
-     * @see getTemporalUnitBetween(fromDate,toDate,unit)
+     * @param date1 first date Object
+     * @param date2 second date Object
+     * @return a long that represent the hours between date1 and date2
+     * @throws ArithmeticException if numeric overflow occurs
+     * @throws IllegalArgumentException Thrown if any of the arguments are null
      */
     public final long getHoursBetween(LocalDateTime date1, LocalDateTime date2)
                 throws ArithmeticException,IllegalArgumentException {
@@ -151,13 +138,27 @@ public class DateUtilites8 {
     
     
     /**
-     * Creates an array of long values that
+     * Calculates the years,months,days,hours,minutes, and seconds in time between two LocalDateTime objects where each value is broken down into an array<br>
+     * Example:<br>
+     * <b>date1</b> = 4/29/1992 7:30:00am<br>
+     * <b>date2</b> = 11/6/2017 9:00:00pm<br>
+     * the return value would be an array: {25,6,8,13,30,0} <br>
+     * meaning: 25 Years, 6 Months, 8 Days, 13 Hours, 36 Minutes, 0 Seconds has passed between date1 and date2
      * 
-     * @param date1 - first date Object
-     * @param date2 - second date Object
-     * @return - 
-     * @throws ArithmeticException - if numeric overflow occurs
-     * @throws IllegalArgumentException - Thrown if any of the arguments are null
+     * @param date1 first date Object
+     * @param date2 second date Object
+     * @return An array of long values where each value represents the additive: 
+     * <ol start="0">
+     * <li>YEARS</li>
+     * <li>MONTHS</li>
+     * <li>DAYS</li>
+     * <li>HOURS</li>
+     * <li>MINUTES</li>
+     * <li>SECONDS</li>
+     * </ol>
+     * between date1 and date2. <br>
+     * @throws ArithmeticException if numeric overflow occurs
+     * @throws IllegalArgumentException Thrown if any of the arguments are null
      */
     public final long[] getChronoUnitBreakDownBetween(LocalDateTime date1, LocalDateTime date2)
             throws ArithmeticException,IllegalArgumentException{
@@ -188,26 +189,43 @@ public class DateUtilites8 {
        return breakDown;
     }
     
-    
-    public final String getChronoUnitBreakDownToString(LocalDateTime date1, LocalDateTime date2) throws IllegalArgumentException{
-        return getChronoUnitBreakDownToString(getChronoUnitBreakDownBetween(date1,date2));
-    }
-    public final String getChronoUnitBreakDownToString(long[] breakDown) throws IllegalArgumentException{
-        if(breakDown == null) throw new IllegalArgumentException("Array May Not Be Null");
-        if(breakDown.length != BREAK_DOWN_UNITS) throw new IllegalArgumentException("Array Must Have Length Of " + BREAK_DOWN_UNITS);
+    /**
+     * Calculates the years,months,days,hours,minutes, and seconds in time between two LocalDateTime objects and outputs results as a formated string<br>
+     * Example:<br>
+     * <b>date1</b> = 4/29/1992 7:30:00am<br>
+     * <b>date2</b> = 11/6/2017 9:00:00pm<br>
+     * would return "25 Years, 6 Months, 8 Days, 13 Hours, 36 Minutes, 0 Seconds"
+     * 
+     * @param date1 first date Object
+     * @param date2 second date Object
+     * @return A String Denoting the "? Years, ? Months, ? Days, ? Hours, ? Minutes, ? Seconds" between date1 and date2
+     * @throws ArithmeticException if numeric overflow occurs
+     * @throws IllegalArgumentException Thrown if any of the arguments are null
+     */
+    public final String getChronoUnitBreakDownAsString(LocalDateTime date1, LocalDateTime date2) throws IllegalArgumentException,ArithmeticException{
+        long[] breakDown = getChronoUnitBreakDownBetween(date1,date2);
         return breakDown[YEARS_IN_ARRAY] + " Years, " +
                breakDown[MONTHS_IN_ARRAY] + " Months, " +
                breakDown[DAYS_IN_ARRAY] + " Days, " +
                breakDown[HOURS_IN_ARRAY] + " Hours, " +
-               breakDown[MINNUTES_IN_ARRAY] + " Minnutes, " +
-               breakDown[SECONDS_IN_ARRAY] + " Seconds";               
+               breakDown[MINNUTES_IN_ARRAY] + " Minutes, " +
+               breakDown[SECONDS_IN_ARRAY] + " Seconds";      
     }
     
-    public final LocalDateTime findNextDayOfWeekWithDateOfMonth(DayOfWeek dow, int dom) throws IllegalArgumentException{
-       return findNextDayOfWeekWithDateOfMonth(LocalDateTime.now(),dow,dom);
-    }
-    
-    public final LocalDateTime findNextDayOfWeekWithDateOfMonth(LocalDateTime date,DayOfWeek dow, int dom) throws IllegalArgumentException{
+    /**
+     * Finds the next LocalDate occurrence where DayOfWeek and DateInMonth overlap<br>
+     * Example:<br>
+     * <b>date</b> = 11/6/2017<br>
+     * <b>dow</b> = DayOfWeek.FRIDAY<br>
+     * <b>dom</b> = 13 <br>
+     * The Next Date with a Friday 13th would be: 04/13/2018
+     * @param date start date to use for search
+     * @param dow the DayOfWeek value desired
+     * @param dom the Day Of Month value desired
+     * @return LocalDate object who's value is the next occurrence of DayOfWeek with DayOfMonth
+     * @throws IllegalArgumentException if date or dow is null OR 31 > dom > 1
+     */
+    public final LocalDate findNextDayOfWeekWithDateOfMonth(LocalDate date,DayOfWeek dow, int dom) throws IllegalArgumentException{
         if(date == null) throw new IllegalArgumentException("Date May Not Be Null");
         if(dow == null) throw new IllegalArgumentException("Day Of Week May Not Be Null");
         if(dom > 31 || dom < 1) throw new IllegalArgumentException("Day Of Month Must Be Between 1-31 inclusive");
@@ -216,14 +234,27 @@ public class DateUtilites8 {
         }while(date.getDayOfMonth() != dom);
         return date;
     }
-
-    public final String formatLocalDateTimeToString(LocalDateTime date)throws IllegalArgumentException{
-        return formatLocalDateTimeToString(date,defaultPattern);
+    
+     /**
+     * Finds the next LocalDate occurrence where DayOfWeek and DateInMonth overlap using LocalDate.now()
+     * as argument for findNextDayOfWeekWithDateOfMonth(LocalDate,DayOfWeek,int)
+     * 
+     * @param dow the DayOfWeek value desired
+     * @param dom the Day Of Month value desired
+     * @return LocalDate object who's value is the next occurrence of DayOfWeek with DayOfMonth
+     * @throws IllegalArgumentException if dow is null OR 31 > dom > 1
+     */
+    public final LocalDate findNextDayOfWeekWithDateOfMonth(DayOfWeek dow, int dom) throws IllegalArgumentException{
+       return findNextDayOfWeekWithDateOfMonth(LocalDate.now(),dow,dom);
     }
     
     public final String formatLocalDateTimeToString(LocalDateTime date, String pattern) throws IllegalArgumentException{
         if(date == null || pattern == null || pattern.isEmpty()) throw new IllegalArgumentException("Arguments May Not Be Null Or Empty String");
         return DateTimeFormatter.ofPattern(pattern).format(date);
+    }
+    
+    public final String formatLocalDateTimeToString(LocalDateTime date)throws IllegalArgumentException{
+        return formatLocalDateTimeToString(date,defaultPattern);
     }
     
     public final LocalDateTime getLocalDateTimeFromString(String dateString) throws IllegalArgumentException{
